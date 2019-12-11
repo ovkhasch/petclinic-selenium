157a811a2eb376bd3c7efb6071db07bbca94a789
ovkhasch <oleg.khaschansky@smddev.com>
2019-12-11 06:34:18 +0300

    Update PetClinicTest.java

--- src/test/java/selenium/automation/test/PetClinicTest.java
+++ src/test/java/selenium/automation/test/PetClinicTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 //import org.openqa.selenium.phantomjs.PhantomJSDriver;
 //import org.openqa.selenium.remote.DesiredCapabilities;
